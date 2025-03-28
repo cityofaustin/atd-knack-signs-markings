6ab61f579b33239266336f16da4cd0dd4daab81c
chiaberry <analuciaby@gmail.com>
2025-03-28 15:23:06 -0500

    hello next world the app

---
+++ signs-work-order-map/README.md
@@ -0,0 +1,3 @@
+# Knack Geolocation Selector
+
+This React app creates a geospatial picker that can display, drop, and edit location pins in the Knack Signs and Markings application. Data about the points is passed between the Knack app and the React app using iframe messaging.
---
+++ signs-work-order-map/app/page.tsx
@@ -0,0 +1,25 @@
+"use client"; // why is it telling me to do this
+import styles from "./page.module.css";
+import MapGL from "react-map-gl/mapbox";
+
+import { DEFAULT_MAP_PARAMS, DEFAULT_MAP_PAN_ZOOM } from "@/config/map";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div className={styles.map}>
+          <MapGL
+            initialViewState={{
+              latitude: DEFAULT_MAP_PAN_ZOOM.latitude,
+              longitude: DEFAULT_MAP_PAN_ZOOM.longitude,
+              zoom: DEFAULT_MAP_PAN_ZOOM.zoom,
+            }}
+            {...DEFAULT_MAP_PARAMS}
+          />
+        </div>
+      </main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
