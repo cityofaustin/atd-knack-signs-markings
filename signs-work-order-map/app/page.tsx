a538e422fc35efe1326770ef50c43ef2d7887738
chiaberry <analuciaby@gmail.com>
2025-04-01 12:15:54 -0500

    update metadata and remove some boilerplate

--- signs-work-order-map/app/page.tsx
+++ signs-work-order-map/app/page.tsx
@@ -1,11 +1,16 @@
 "use client"; // why is it telling me to do this
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import styles from "./page.module.css";
-import MapGL, { ViewStateChangeEvent } from "react-map-gl/mapbox";
+import MapGL, { Marker, ViewStateChangeEvent } from "react-map-gl/mapbox";
 
 import GeocoderControl from "@/components/MapGeocoderControl";
 
 import { DEFAULT_MAP_PARAMS, DEFAULT_MAP_PAN_ZOOM } from "@/config/map";
+
+interface LatLon {
+  latitude: number;
+  longitude: number;
+}
 
 export default function Home() {
   const onMoveEnd = useCallback((e: ViewStateChangeEvent) => {
@@ -13,11 +18,16 @@ export default function Home() {
     const latitude = +e.viewState.latitude.toFixed(8);
     const longitude = +e.viewState.longitude.toFixed(8);
     console.log(latitude, longitude);
-    // setMapLatLon({
-    //   latitude,
-    //   longitude,
-    // });
+    setMapLatLon({
+      latitude,
+      longitude,
+    });
   }, []);
+
+  const [mapLatLon, setMapLatLon] = useState<LatLon>({
+    latitude: DEFAULT_MAP_PAN_ZOOM.latitude,
+    longitude: DEFAULT_MAP_PAN_ZOOM.longitude,
+  });
 
   return (
     <div className={styles.page}>
@@ -32,7 +42,11 @@ export default function Home() {
             {...DEFAULT_MAP_PARAMS}
             onMoveEnd={onMoveEnd}
           >
-            {/* do i include the marker here */}
+            <Marker
+              longitude={mapLatLon.longitude}
+              latitude={mapLatLon.latitude}
+              // draggable
+            />
             <GeocoderControl position="top-left" marker={true} />
           </MapGL>
         </div>
