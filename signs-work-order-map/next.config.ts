5ac3bfbfad783b945e50a037439eab126e1bc8f6
chiaberry <analuciaby@gmail.com>
2025-04-02 10:16:22 -0500

    include the env template

--- signs-work-order-map/next.config.ts
+++ signs-work-order-map/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: "export", // Forces static export (acts like a SPA)
   reactStrictMode: true, // Enables React Strict Mode
   trailingSlash: true, // Ensures static file paths end with `/`
-  swcMinify: true, // Optimizes JavaScript
+  // swcMinify: true, // Optimizes JavaScript -- this gave me an error
 };
 
 export default nextConfig;
