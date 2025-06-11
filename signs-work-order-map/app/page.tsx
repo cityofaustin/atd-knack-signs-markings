d41859117d7879767e24d5f22cd677681bc04a20
chiaberry <analuciaby@gmail.com>
2025-06-11 10:42:58 -0500

    move format signs into utils file

--- signs-work-order-map/app/page.tsx
+++ signs-work-order-map/app/page.tsx
@@ -1,38 +1,19 @@
 "use client";
-import { useMemo } from "react";
 import styles from "./page.module.css";
 import Map from "@/components/Map";
 import { KnackRecord, Sign, KnackToIFrameMessage } from "@/types/map";
 import { useIFrameMessenger } from "@/utils/iFrameMessenger";
+import { formatSignsRecords } from "@/utils/mapUtils";
 
-const formatSignsRecords = (
-  knackPayload: KnackToIFrameMessage | null
-): Sign[] =>
-  useMemo(() => {
-    if (!knackPayload) return [];
-    if (
-      knackPayload?.message === "EDIT_LOCATION" ||
-      knackPayload?.message === "KNACK_GEOLOCATION"
-    ) {
-      return [];
-    }
-    console.log(knackPayload?.payload?.records);
-    return knackPayload.payload.records.map((sign) => ({
-      id: sign.id,
-      lat: sign.field_3300_raw.latitude,
-      lng: sign.field_3300_raw.longitude,
-      spatialId: sign.field_3297,
-      workOrderId: knackPayload.payload.workOrderId,
-    }));
-  }, [knackPayload]);
+
 
 export default function Home() {
   const knackPayload = useIFrameMessenger();
-  // const knackPayload = testpayload;
 
   console.log(knackPayload);
 
-  const location = knackPayload?.payload?.location;
+  const location = knackPayload?.payload?.location; // turn this into a format location function
+  // because we should zoom to location if it exists
   const signs = formatSignsRecords(knackPayload);
   console.log(signs);
   const messageType = knackPayload?.message;
--- signs-work-order-map/utils/mapUtils.tsx
+++ signs-work-order-map/utils/mapUtils.tsx
@@ -1,11 +1,11 @@
 import { useMemo } from "react";
 import bbox from "@turf/bbox";
 import { lineString } from "@turf/helpers";
-import { Sign } from "@/types/map";
+import { Sign, KnackToIFrameMessage } from "@/types/map";
 import { LngLatBoundsLike } from "mapbox-gl";
 
 /**
- *
+ * Takes array of Signs and if signs exist, returns bounding box for signs
  * @param signs
  * @returns
  */
@@ -14,13 +14,42 @@ export const formatBounds = (signs: Sign[]): undefined | LngLatBoundsLike =>
     if (signs.length === 0) {
       return undefined;
     }
-    const signArray = signs.map((sign: Sign) => [sign.lng, sign.lat]);
+    const signLatLonArray = signs.map((sign: Sign) => [sign.lng, sign.lat]);
 
     const lineStringFeature =
-      signArray.length < 2
-        ? lineString([signArray[0], [signArray[0][0], signArray[0][1]]])
-        : lineString(signArray);
+      // if there is only one sign in the array, create the linestring for the bounding box using the one sign
+      signLatLonArray.length < 2
+        ? lineString([
+            signLatLonArray[0],
+            [signLatLonArray[0][0], signLatLonArray[0][1]],
+          ])
+        : lineString(signLatLonArray);
 
     const [minLng, minLat, maxLng, maxLat] = bbox(lineStringFeature);
     return [minLng, minLat, maxLng, maxLat];
   }, [signs]);
+
+/**
+ * Function that takes data from knack app and depending on message type, returns array of signs or empty array
+ * @param knackPayload - message from Knack via IFrameMessage
+ * @returns Array of Signs or empty array if no sign data
+ */
+export const formatSignsRecords = (
+  knackPayload: KnackToIFrameMessage | null
+): Sign[] =>
+  useMemo(() => {
+    if (!knackPayload) return [];
+    if (
+      knackPayload?.message === "EDIT_LOCATION" ||
+      knackPayload?.message === "KNACK_GEOLOCATION"
+    ) {
+      return [];
+    }
+    return knackPayload.payload.records.map((sign) => ({
+      id: sign.id,
+      lat: sign.field_3300_raw.latitude,
+      lng: sign.field_3300_raw.longitude,
+      spatialId: sign.field_3297,
+      workOrderId: knackPayload.payload.workOrderId,
+    }));
+  }, [knackPayload]);
