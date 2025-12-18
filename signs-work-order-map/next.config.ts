d39d72451d5e73e712774697662c8c4fcc2a4586
John Clary <john.clary@austintexas.gov>
2025-12-18 16:16:37 -0500

    updates to reenable linting

--- signs-work-order-map/next.config.ts
+++ signs-work-order-map/next.config.ts
@@ -1,9 +1,13 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Enables React Strict Mode
   trailingSlash: true, // Ensures static file paths end with `/`
   // swcMinify: true, // Optimizes JavaScript -- this gave me an error
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
